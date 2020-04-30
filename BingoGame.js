6e6c444a936dfb07051cbcccbc9d0e4af49db697
KohsukeAraki <64396950+KohsukeAraki@users.noreply.github.com>
2020-04-30 11:06:40 +0900

    Show on Random

--- BingoGame.js
+++ BingoGame.js
@@ -1,4 +1,18 @@
 document.getElementById('start').onclick = function(){
+    var min = 1;
+    var max = 75;
+    var bingo = [];
+    for(var i = min;i <= max;i++){
+        bingo.push(i);
+    }
+
+    for(var i = bingo.length - 1; i > 0; i--){
+        var r = Math.floor(Math.random() * (i + 1));
+        var tmp = bingo[i];
+        bingo[i] = bingo[r];
+        bingo[r] = tmp;
+    }
+
     var table = document.createElement('table');
     var tr = document.createElement('tr');
     var th = document.createElement('th');
@@ -11,10 +25,8 @@ document.getElementById('start').onclick = function(){
     var td = document.createElement('td');
     var tr = document.createElement('tr');
 
-    var min = 1;
-    var max = 75;
     
-    var BingoNum = Math.floor(Math.random() * (max + 1 - min)) + min;
+    var BingoNum = bingo.pop();
     td.textContent = BingoNum;
     tr.appendChild(td);
     table.appendChild(tr);
