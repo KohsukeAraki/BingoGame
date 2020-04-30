421417d58077eac2c09dd21017944d124529a3f1
KohsukeAraki <64396950+KohsukeAraki@users.noreply.github.com>
2020-04-30 16:43:14 +0900

    リセット機能を追加しました

--- BingoGame.js
+++ BingoGame.js
@@ -1,18 +1,22 @@
+var min = 1;
+var max = 75;
+var count = 0;
+var bingo = [];
+
+for(var i = min;i <= max;i++){
+    bingo.push(i);
+}
+
+for(var i = bingo.length - 1; i > 0; i--){
+    var r = Math.floor(Math.random() * (i + 1));
+    var tmp = bingo[i];
+    bingo[i] = bingo[r];
+    bingo[r] = tmp;
+}
+
+
 document.getElementById('start').onclick = function(){
-    var min = 1;
-    var max = 75;
-    var bingo = [];
-    for(var i = min;i <= max;i++){
-        bingo.push(i);
-    }
-
-    for(var i = bingo.length - 1; i > 0; i--){
-        var r = Math.floor(Math.random() * (i + 1));
-        var tmp = bingo[i];
-        bingo[i] = bingo[r];
-        bingo[r] = tmp;
-    }
-
+ 
     var table = document.createElement('table');
     var tr = document.createElement('tr');
     var th = document.createElement('th');
@@ -24,8 +28,8 @@ document.getElementById('start').onclick = function(){
 
     var td = document.createElement('td');
     var tr = document.createElement('tr');
-
-    
+if(count != 75){
+    count++;
     var BingoNum = bingo.pop();
     td.textContent = BingoNum;
     tr.appendChild(td);
@@ -44,6 +48,35 @@ document.getElementById('start').onclick = function(){
         }
         tr.appendChild(td);
         table.appendChild(tr);
+    }else{
+        td.textContent = "終了!";
+        alert("全部出し終わりました");
+        tr.appendChild(td);
+        table.appendChild(tr);
+    }
         document.getElementById('BingoTable').appendChild(table);
     
-} 
+}
+
+document.getElementById('reset').onclick = function(){
+    var res = confirm("リセットしても構いませんか？");
+    if(res == true){
+        bingo = [];
+
+        for(var i = min;i <= max;i++){
+            bingo.push(i);
+        }
+        
+        for(var i = bingo.length - 1; i > 0; i--){
+            var r = Math.floor(Math.random() * (i + 1));
+            var tmp = bingo[i];
+            bingo[i] = bingo[r];
+            bingo[r] = tmp;
+        }
+        count = 0;
+        var dom_obj = document.getElementById('BingoTable');
+        while (dom_obj) dom_obj.removeChild(dom_obj.firstChild);
+
+    }
+}
+
